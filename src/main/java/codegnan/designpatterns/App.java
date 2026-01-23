d281c57ccba06b20714c6e48b9c0c462fb518861
sudheercg <sudheer@codegnan.com>
2026-01-23 10:01:47 +0530

    Updated with more text App.java

--- src/main/java/codegnan/designpatterns/App.java
+++ src/main/java/codegnan/designpatterns/App.java
@@ -5,6 +5,6 @@ package codegnan.designpatterns;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Codeman!");
+        System.out.println("Hello Codeman , great man!");
     }
 }
